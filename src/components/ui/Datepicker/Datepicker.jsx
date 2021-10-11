6050d56dea87ffddad60ba1ab603b803bc88c033
Chris Torres <chris.mood92@gmail.com>
2021-10-11 12:55:34 -0500

    Add date picker and form

--- src/components/ui/Datepicker/Datepicker.jsx
+++ src/components/ui/Datepicker/Datepicker.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import { useField } from 'formik';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-function DatePickerComp() {
-  const [startDate, setStartDate] = useState(new Date());
-  return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-  );
+function DatePickerComp(props) {
+  const [field] = useField(props);
+
+  return <DatePicker {...field} {...props} />;
 }
 
 export default DatePickerComp;
--- src/components/ui/RegisterForm/RegisterForm.jsx
+++ src/components/ui/RegisterForm/RegisterForm.jsx
@@ -1,10 +1,45 @@
-import DatePicker from '@/components/ui/Datepicker/Datepicker';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+import Datepicker from '@/components/ui/Datepicker/Datepicker';
+
 import styles from './RegisterForm.module.scss';
 
 function RegisterForm() {
   return (
     <div className={styles.form_container}>
-      <DatePicker />
+      <Formik
+        initialValues={{ birthDate: new Date() }}
+        validate={(values) => {
+          const errors = {};
+          console.log(values);
+          if (!values.birthDate) {
+            errors.birthDate = 'Required';
+          }
+          // TODO: add here the date validation
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting, values, setFieldValue }) => (
+          <Form>
+            <Datepicker
+              name="birthDate"
+              value={values?.birthDate}
+              selected={values?.birthDate}
+              onChange={(e) => setFieldValue('birthDate', e)}
+            />
+            <ErrorMessage name="birthDate" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
